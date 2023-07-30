97d3cfc5abc1e9c059b5d435d46facbe6447574d
Abror <khaitboboev94@mail.ru>
2023-07-31 01:46:40 +0300

    best sale day

---
+++ untitled/src/algo_task_2/BestSaleDay.java
@@ -0,0 +1,14 @@
+package algo_task_2;
+
+public class BestSaleDay {
+
+    private static int saleDay(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int startPrice = prices[0];
+        int profit = 0;
+
+        for (int i =0; i < prices.length; i++) {
+            
+        }
+    }
+}
