f69eae213cb63205f2610ba8be38cf5f57a4a35f
Abror <khaitboboev94@mail.ru>
2023-08-02 20:12:38 +0300

    fixed time complexity

--- untitled/src/algo_task_2/FindDuplicates.java
+++ untitled/src/algo_task_2/FindDuplicates.java
@@ -1,23 +1,25 @@
 package algo_task_2;
 
+import javax.swing.*;
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindDuplicates {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1,2,3,4,1};
         System.out.println(duplicate(nums));
     }
 
     private static boolean duplicate(int[] nums) {
         if(nums == null || nums.length == 0) return false;
-        int count = 0;
 
-        for (int i =0; i < nums.length; i++) {
-         for (int j =i +1; j < nums.length; j++) {
-             if (nums[i] == nums[j]) count++;
+        Set<Integer>  set = new HashSet<>();
+        for (int i : nums) {
+         if (!set.add(i)) {
+             return true;
          }
         }
-
-        if(count > 0 ) return true;
         return false;
     }
 }
